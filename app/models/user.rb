1f736bb4c1f96f80615c3caca5014736819fa724
Brady Edwards <brady.edwards16@gmail.com>
2023-11-16 14:33:28 -0500

    Submit button bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@
 # password_confirmation:string virtual
 class User < ApplicationRecord
     has_secure_password
+
+    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email address" }
 end
