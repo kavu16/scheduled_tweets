bd9bbfdd0ffd6b16dd4fc917c941b3a64d9e2ca9
Brady Edwards <brady.edwards16@gmail.com>
2023-11-17 13:08:54 -0500

    Password Change/Reset

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,15 @@ Rails.application.routes.draw do
   # GET /about
   get "about", to: "about#index"
 
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
 
